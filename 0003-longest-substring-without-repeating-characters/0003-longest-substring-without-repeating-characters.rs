2b6c338d596e29126c8874a7befb7406afb4103a
Divya Bhat <84025524+divya-r-b@users.noreply.github.com>
2023-09-17 16:49:11 +0530

    Update 0003-longest-substring-without-repeating-characters.rs

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.rs
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.rs
@@ -1,10 +1,11 @@
- use std::collections::HashMap;
+//sliding window approach
+use std::collections::HashMap;
  impl Solution {
     pub fn length_of_longest_substring(s: String) -> i32 {
     
-    let mut char_index_map: HashMap<char, usize> = HashMap::new();
-    let mut max_length = 0;
-    let mut start_index = 0;
+    let mut char_index_map: HashMap<char, usize> = HashMap::new();//store characters as keys and their corresponding indices in the string as values.
+    let mut max_length = 0;//This variable keeps track of the maximum length of the substring without repeating characters encountered so far.
+    let mut start_index = 0;//This variable keeps track of the start of the current substring without repeating characters.
 
     for (end_index, c) in s.chars().enumerate() {
         if let Some(&prev_index) = char_index_map.get(&c) {
